5c4cbeb0dc7f2083afa3806e95c4862fc79fa6cf
chess <szk.c22+git@gmail.com>
2017-05-31 07:02:25 +0900

    refact

--- DQN_tf.py
+++ DQN_tf.py
@@ -2,10 +2,8 @@
 from collections import deque
 import os
 
-
 import numpy as np
 import time
-
 
 import tensorflow as tf
 
@@ -35,6 +33,17 @@ class DQNAgent():
         self.discount_factor = 0.9
         self.exploration = 0.1
         self.epsilon = epsilon
+        self.experienceMemory=[] # 経験メモリ（エピソードローカル）
+        self.experienceMemory_local=[] # 経験メモリ（エピソードローカル）
+        self.memSize = 300*100  # 経験メモリのサイズ(300サンプリングx100エピソード)
+        self.experienceMemory_local=[] # 経験メモリ（エピソードローカル）
+        self.memPos = 0 #メモリのインデックス
+        self.batch_num = 32 # 学習に使うバッチサイズ
+        self.gamma = 0.9       # 割引率
+        self.loss=0
+        self.total_reward_award=np.ones(100)*-1000 #100エピソード
+
+
 
         # replay memory
         self.D = deque(maxlen=self.replay_memory_size)
@@ -47,24 +56,24 @@ class DQNAgent():
 
     def init_model(self):
 
-        # input layer (8 x 8)
-        self.x = tf.placeholder(tf.float32, [None, 4, 4])
+        # input layer (1 x 4)
+        self.x = tf.placeholder(tf.float32, [4])
 
         # flatten (64)
-        x_flat = tf.reshape(self.x, [-1, 16])
+        x_flat = tf.reshape(self.x, [-1, 4])
 
         # fully connected layer (32)
-        W_fc1 = tf.Variable(tf.truncated_normal([16, 32], stddev=0.01))
-        b_fc1 = tf.Variable(tf.zeros([32]))
+        W_fc1 = tf.Variable(tf.truncated_normal([4 ,16], stddev=0.01))
+        b_fc1 = tf.Variable(tf.zeros([16]))
         h_fc1 = tf.nn.relu(tf.matmul(x_flat, W_fc1) + b_fc1)
 
         # output layer (n_actions)
-        W_out = tf.Variable(tf.truncated_normal([32, self.n_actions], stddev=0.01))
+        W_out = tf.Variable(tf.truncated_normal([16, self.n_actions], stddev=0.01))
         b_out = tf.Variable(tf.zeros([self.n_actions]))
         self.y = tf.matmul(h_fc1, W_out) + b_out
 
         # loss function
-        self.y_ = tf.placeholder(tf.float32, [None, self.n_actions])
+        self.y_ = tf.placeholder(tf.float32, [self.n_actions])
         self.loss = tf.reduce_mean(tf.square(self.y_ - self.y))
 
         # train operation
@@ -81,7 +90,7 @@ class DQNAgent():
     def get_action_value(self, state):
         #Q_values(self, state): #
         # Q(state, action) of all actions
-        return self.sess.run(self.y, feed_dict={self.x: [state]})[0]
+        return self.sess.run(self.y, feed_dict={self.x: state})[0]
 
     def reduce_epsilon(self):
         self.epsilon-=1.0/100000
@@ -97,14 +106,43 @@ class DQNAgent():
             # max_action Q(state, action)
             return self.enable_actions[np.argmax(self.get_action_value(state))]
 
+    def experience_local(self,old_seq, action, reward, new_seq):
+        #エピソードローカルな記憶
+        self.experienceMemory_local.append( np.hstack([old_seq,action,reward,new_seq]) )
+
     def store_experience(self, state, action, reward, state_1, train):
         self.D.append((state, action, reward, state_1, train))
+
+
+    def experience_global(self,total_reward):
+        #グローバルな記憶
+        #ベスト100に入る経験を取り込む
+        if np.min(self.total_reward_award)<total_reward:
+            i=np.argmin(self.total_reward_award)
+            self.total_reward_award[i]=total_reward
+
+            # GOOD EXPERIENCE REPLAY
+            for x in self.experienceMemory_local:
+                self.experience( x )
+
+        #一定確率で優秀でないものも取り込む
+        if np.random.random()<0.01:
+            # # NORMAL EXPERIENCE REPLAY
+            for x in self.experienceMemory_local:
+                self.experience( x )
+
+        self.experienceMemory_local=[]
+
+    def experience(self,x):
+        if len(self.experienceMemory)>self.memSize:
+            self.experienceMemory[int(self.memPos%self.memSize)]=x
+            self.memPos+=1
+        else:
+            self.experienceMemory.append( x )
 
     def experience_replay(self):
         state_minibatch = []
         y_minibatch = []
-        seq1=[]
-        seq1=np.zeros(4,4)
 
         # sample random minibatch
         minibatch_size = min(len(self.D), self.minibatch_size)
@@ -113,8 +151,8 @@ class DQNAgent():
         for j in minibatch_indexes:
             state_j, action_j, reward_j, state_j_1, terminal = self.D[j]
             action_j_index = self.enable_actions.index(action_j)
-
-            y_j = self.get_action_value(seq1)
+            state_j
+            y_j = self.get_action_value(state_j)
 
             if terminal:
                 y_j[action_j_index] = reward_j
@@ -132,33 +170,60 @@ class DQNAgent():
         self.current_loss = self.sess.run(self.loss, feed_dict={self.x: state_minibatch, self.y_: y_minibatch})
 
 
-'''
-#DQNアルゴリズムにしたがって動作するエージェント
-class DQNAgent():
-    def __init__(self, epsilon=0.99):
-        self.model = Q()
-        self.optimizer = tf.train.AdamOptimizer(1e-4)
-        self.optimizer.setup(self.model)
-        self.epsilon = epsilon # ランダムアクションを選ぶ確率
-        self.actions=[0,1] #　行動の選択肢
-        self.experienceMemory = [] # 経験メモリ
-        self.memSize = 300*100  # 経験メモリのサイズ(300サンプリングx100エピソード)
-        self.experienceMemory_local=[] # 経験メモリ（エピソードローカル）
-        self.memPos = 0 #メモリのインデックス
-        self.batch_num = 32 # 学習に使うバッチサイズ
-        self.gamma = 0.9       # 割引率
-        self.loss=0
-        self.total_reward_award=np.ones(100)*-1000 #100エピソード
-'''
+    def update_model(self,old_seq, action, reward, new_seq):
+        '''
+        モデルを更新する
+        '''
+        # 経験メモリにたまってない場合は更新しない
+        if len(self.experienceMemory)<self.batch_num:
+            return
+
+        # 経験メモリからバッチを作成
+        memsize=len(self.experienceMemory)
+        batch_index = list(np.random.randint(0,memsize,(self.batch_num)))
+        batch =np.array( [self.experienceMemory[i] for i in batch_index ])
+        x = batch[:,0:STATE_NUM].reshape( (self.batch_num,-1)).astype(np.float32)
+        # print(x)
+        targets=[]
+        # targets=np.zeros((x.shape[0],4))
+        # targets=np.array([self.get_action_value(x[j]) for i in x.shape[0]])
+        for j in range(x.shape[0]):
+        #   targets[j]=self.get_action_value(x[j])
+            targets.append(np.ndarray.tolist(self.get_action_value(x[j])))
+        #targets=self.get_action_value(x).data.copy()
+        targets = np.array(targets)
+        # print("targets=",targets)
+
+        for i in range(self.batch_num):
+            #[ seq..., action, reward, seq_new]
+            a = batch[i,STATE_NUM]
+            r = batch[i, STATE_NUM+1]
+            ai=int((a+1)/2) #±1 をindex(0,1)に。
+            new_seq= batch[i,(STATE_NUM+2):(STATE_NUM*2+2)]
+            targets[i,ai]=( r+ self.gamma * np.max(self.get_action_value(new_seq)))
+        t =targets
+        #t = Variable(np.array(targets).reshape((self.batch_num,-1)).astype(np.float32))
 
 
+        # ネットの更新
+        # self.model.zerograds()
+        # loss=self.model(x ,t)
+        # self.loss = loss.data
+        # loss.backward()
+        # self.optimizer.update()
 
-
+        # training
+        for i in range(x.shape[0]):
+            x1 = x[i]
+            t2 = t[i]
+            self.sess.run(self.training, feed_dict={self.x: x1, self.y_: t2})
+            # for log
+            self.current_loss = self.sess.run(self.loss, feed_dict={self.x: x1 ,self.y_: t2})
 
 
 class pendulumEnvironment():
     '''
-    振り子環境。
+    model
     '''
     def __init__(self):
         self.env = wrappers.Monitor(gym.make('CartPole-v0'), './private/tmp/cartpole-experiment-3', force = True)
@@ -214,18 +279,19 @@ class simulator:
 
             # エピソードローカルなメモリに記憶する
             self.agent.store_experience(old_seq, action, reward, new_seq,train)
+            self.agent.experience_local(old_seq, action, reward, new_seq)
 
             if done:
                 print("Episode finished after {} timesteps".format(i+1))
                 break
 
         # エピソードローカルなメモリ内容をグローバルなメモリに移す
-        # self.agent.experience_global(total_reward)
+        self.agent.experience_global(total_reward)
 
         if train:
             # 学習用メモリを使ってモデルを更新する
-            # self.agent.update_model(old_seq, action, reward, new_seq)
-            self.agent.experience_replay()
+            self.agent.update_model(old_seq, action, reward, new_seq)
+            # self.agent.experience_replay()
             self.agent.reduce_epsilon()
 
         return total_reward
@@ -236,7 +302,7 @@ if __name__ == '__main__':
     sim=simulator(env,agent)
 
     best_reward1 = 0
-    for i in range(10):
+    for i in range(1000):
         total_reward1 = sim.run(train=True)
         if best_reward1 < total_reward1:
             best_reward1 = total_reward1
@@ -248,4 +314,4 @@ if __name__ == '__main__':
             break
 
     env.monitor_close()
-    #gym.upload('./private/tmp/cartpole-experiment-3', api_key='sk_GDB9izzTxu1PyxNAdhcw')
+    gym.upload('./private/tmp/cartpole-experiment-3', api_key='sk_GDB9izzTxu1PyxNAdhcw')
